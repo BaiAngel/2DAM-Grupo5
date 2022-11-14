c78d3c6eb3a3f89733cb6bd4d90c6340e51a1a5a
angel <a19angavimar@inspedralbes.cat>
2022-11-14 11:40:29 +0100

    home page

--- app/src/main/java/com/example/a2dam_grupo5/MainActivity2.java
+++ app/src/main/java/com/example/a2dam_grupo5/MainActivity2.java
@@ -2,13 +2,24 @@ package com.example.a2dam_grupo5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
+    private static final String LOG_TAG =
+            MainActivity2.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void launchFirstactivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
